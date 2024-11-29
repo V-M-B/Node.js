5d254f843285ba1bd64c1df2a7053545d356c171
V-M-B <vmbvarun@gmail.com>
2024-11-29 12:37:45 +0530

    05-Handling URL's

--- 05-Handling URL's/index.js
+++ 05-Handling URL's/index.js
@@ -1,16 +1,19 @@
-const http = require("http");
 const fs = require("fs");
+const http = require("http");
 
 const myServer = http.createServer((req, res) => {
-  // console.log(req.headers);
-  // console.log("Request Received!");
+  if (req.url === "/favicon.ico") return res.end();
 
-//   not printing fav icon req
-  if (req.url === "/favicon.ico") {
-    return res.end();
-  }
-  const log = `A request received on ${req.url} at ${Date.now()}\n`;
-  fs.appendFile("log.txt", log, (err, data) => {
+  const log = `${Date.now()}: ${req.url} New Req received\n`;
+
+  fs.appendFile("log.txt", log, (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+      res.statusCode = 500; // Internal Server Error
+      return res.end("Internal Server Error");
+    }
+
+    // Handle different routes
     switch (req.url) {
       case "/":
         res.end("Home Page");
@@ -21,12 +24,12 @@ const myServer = http.createServer((req, res) => {
         break;
 
       default:
-        res.end("404 Not found");
+        res.end("404 Not Found");
         break;
     }
   });
 });
 
 myServer.listen(3000, () => {
-  console.log("Server started");
+  console.log("Server started on port 3000");
 });
