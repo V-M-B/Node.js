261c8f90c60b319ed7d9e494d874a8e4d0d0ff0a
V-M-B <vmbvarun@gmail.com>
2025-12-24 23:43:55 +0530

    feat: implement authentication routes and controller for user registration, login, and logout

---
+++ Backend Refresh/src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { register, login, logout } from "../controllers/authController.js";
+
+const router = express.Router();
+
+// to register a user 
+router.post("/register", register);
+
+// 
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+export default router;
--- Backend Refresh/src/routes/movieRoutes.js
+++ Backend Refresh/src/routes/movieRoutes.js
@@ -1,10 +1,21 @@
-// const express = require('express');  method 1 
-import express from 'express';  // method 2 (ES6)
+import express from "express";
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.json("Get all movies");
+router.get("/", (req, res) => {
+  res.json({ httpMethod: "get" });
 });
 
-export default router ;
+router.post("/", (req, res) => {
+  res.json({ httpMethod: "post" });
+});
+
+router.put("/", (req, res) => {
+  res.json({ httpMethod: "put" });
+});
+
+router.delete("/", (req, res) => {
+  res.json({ httpMethod: "delete" });
+});
+
+export default router;
